b46afdd0d08eef34194a2401f0dad30f7832a4ab
Walid Sassi <walid@imac-de-walid.local>
2017-01-05 21:00:36 +0100

    Authentification with CoreData NSDescriptionEntity RequestFetchResult

--- AuthentificationWithCoreData/ViewController.swift
+++ AuthentificationWithCoreData/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
-
+// don't foget to import
+import CoreData
 class ViewController: UIViewController {
 
+    @IBOutlet weak var login: UITextField!
+    @IBOutlet weak var passwd: UITextField!
+    @IBOutlet weak var message: UILabel!
+    @IBOutlet weak var btConnect: UIButton!
+    
+    
+    // verify login and password
+    @IBAction func logIn(_ sender: UIButton) {
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appdelegate.persistentContainer.viewContext
+        let newvalue = NSEntityDescription.insertNewObject(forEntityName: "User", into: context)
+        newvalue.setValue(login.text, forKey: "login")
+        newvalue.setValue(passwd.text, forKey: "passwd")
+        do{
+            try context.save()
+            login.alpha = 0
+            passwd.alpha = 0
+            message.alpha = 1
+            btConnect.alpha = 0
+            message.text = "Hi " + login.text! + "!"
+        }
+        catch{
+          print("error in saved")
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // configure connection to coreData
+        let appdelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appdelegate.persistentContainer.viewContext
+        // define the fetchrequest object
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+        
+        // execute context to fetch data from User table
+        do{
+        let results = try context.fetch(request)
+            for result in results as![NSManagedObject]{
+                if let name = result.value(forKey: "login") as? String {
+                    login.alpha = 0
+                    passwd.alpha = 0
+                    message.alpha = 1
+                    btConnect.alpha = 0
+                    message.text = "Hi " + name + "!"
+                }else{
+                    
+                }
+            }
+            
+        }
+        catch {
+            print("there was an error")
+        }
     }
 
     override func didReceiveMemoryWarning() {
